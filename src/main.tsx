651c81eacff53c0825f9d9841fc03f97b24a38c8
Deepak kumar <deepakdusa07@gmail.com>
2025-06-19 10:59:43 +0530

    Protfolio

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
+import React from 'react';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <HashRouter>
+    <App />
+  </HashRouter>
+);
